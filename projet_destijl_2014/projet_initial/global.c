c18f95f4089ad0c44dad51d0bf6f3f129cc3f7ed
Mme Bazile Chloe <cbasile@geitp109-1.insa-toulouse.fr>
2014-04-18 14:00:00 +0200

    fisrt repository commit

---
+++ projet_destijl_2014/projet_initial/global.c
@@ -0,0 +1,37 @@
+/*
+ * File:   global.h
+ * Author: pehladik
+ *
+ * Created on 21 avril 2011, 12:14
+ */
+
+#include "global.h"
+
+RT_TASK tServeur;
+RT_TASK tconnect;
+RT_TASK tmove;
+RT_TASK tenvoyer;
+RT_TASK tcamera;
+
+RT_MUTEX mutexEtat;
+RT_MUTEX mutexMove;
+
+RT_SEM semConnecterRobot;
+
+RT_QUEUE queueMsgGUI;
+
+int etatCommMoniteur = 1;
+int etatCommRobot = 1;
+DRobot *robot;
+DMovement *move;
+DServer *serveur;
+DCamera *camera;
+
+
+int MSG_QUEUE_SIZE = 10;
+
+int PRIORITY_TSERVEUR = 30;
+int PRIORITY_TCONNECT = 20;
+int PRIORITY_TMOVE = 10;
+int PRIORITY_TENVOYER = 25;
+int PRIORITY_TCAMERA = 5;
